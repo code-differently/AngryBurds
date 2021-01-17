6637df9eb0dc67a4de06355f6c7207bf5ac0ad36
Amarachukwu Ugoanusi <nkanusi@gmail.com>
2021-01-16 23:23:50 -0500

    first commit on Angry Burd

--- JavaScript.js
+++ JavaScript.js
@@ -2,20 +2,42 @@ let time = 0;
 
 
 const img = document.getElementById("kface");
-let xSpeed = document.getElementById('hspeed').value;
-let vSpeed = document.getElementById('vspeed');
+const birdhouse = document.getElementById("bhouse");
+const birdhouseWidth = birdhouse.getBoundingClientRect();
+
+
 const launchbtn = document.getElementById('launchbtn');
 
 
-function myFunc(){
-    
-    img.style.left= 900 * time + "px";
-    img.style.bottom =window.innerHeight - (300 * time - .5 * 9.8 * Math.pow(time,2) ) + "px";
-    
-    time++;
+launchbtn.addEventListener('click',function myFunc(){
+  
+  
+    let xSpeed = document.getElementById('hspeed').value;
+    let vSpeed = document.getElementById('vspeed').value;
 
+if(xSpeed == 600 && vSpeed == 200 ){
+    img.style.left= xSpeed * time + "px";
+    img.style.bottom =window.innerHeight - (vSpeed * time - .5 * 9.8 * Math.pow(time,2) ) + "px";
     
+
+    time++;
+    setInterval(myFunc, 600);
+}
+else {
+    let message = document.getElementById("youmissed");
+    img.style.left= xSpeed * time + "px";
+    img.style.bottom =window.innerHeight - (vSpeed * time - .5 * 9.8 * Math.pow(time,2) ) + "px";
+    
+
+    time++;
+    setInterval(myFunc, 600);
+    return message.innerHTML="You Missed";
+
 }
 
-setInterval(myFunc, 1000);
-console.log(xSpeed);
+});
+
+function resetFunction(){
+    location.reload();
+
+}
